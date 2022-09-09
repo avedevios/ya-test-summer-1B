79daf902ca9d4ae9b48c801f382a165a80f36d9c
avedevios <106875633+avedevios@users.noreply.github.com>
2022-09-09 12:21:41 +0300

    First (WA)

--- ya-test-summer-1B/main.swift
+++ ya-test-summer-1B/main.swift
@@ -7,5 +7,69 @@
 
 import Foundation
 
-print("Hello, World!")
+var str = "/../"
+
+//str = readLine()!
+
+func deleteSlash(str: String) -> String {
+    var ans = ""
+    var countSlash = 0
+    var firstSimbol = true
+    
+    for sim in str {
+        if firstSimbol {
+            if sim != "/" {
+                ans.append("/")
+            }
+            firstSimbol = false
+        }
+        
+        if sim == "/" {
+            if countSlash < 1 {
+                ans.append("/")
+                countSlash += 1
+            }
+        } else {
+            ans.append(sim)
+            countSlash = 0
+        }
+    }
+    
+    return ans
+}
+
+if str.count > 0 {
+    
+    if str.first != "/" {
+        str = "/" + str
+    }
+
+    str += "/"
+    var flag = true
+    while flag {
+        if str.contains("/./") {
+            str = str.replacingOccurrences(of: "/./", with: "/")
+        } else if str.contains("//") {
+            str = str.replacingOccurrences(of: "//", with: "/")
+        } else if str.contains("/../") {
+            for i in 0...str.count - 1 {
+                if String(str[i] + str[i+1] + str[i+2] + str[i+3]) == "/../" {
+                    break
+                }
+            }
+            str = str[i + 3>...]
+        } else {
+            flag = false
+        }
+    }
+    
+    if str.count > 1 && str.last! == "/" {
+        print(str.dropLast())
+    } else {
+        print (str)
+    }
+} else {
+    print ("/")
+}
+
 
